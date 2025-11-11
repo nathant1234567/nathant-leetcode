2eec87fb3ed8fb2b70070a820e7b58c67f487af5
NathanT <nathanthompson256@outlook.com>
2025-11-11 15:35:11 +0000

    twosum

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -3,10 +3,10 @@
  * Link: https://leetcode.com/problems/two-sum
  *
  * Approach:
- *  - Briefly describe your approach (e.g., "Use HashMap for complement lookup")
+ *  - Use a HashMap to find complements of numbers in one pass.
  *
- * Time Complexity: O(...)
- * Space Complexity: O(...)
+ * Time Complexity: O(n) (well n^2 super worst case)
+ * Space Complexity: O(n)
  */
 
 package arrays;
@@ -14,13 +14,26 @@ package arrays;
 import java.util.*;
 
 public class TwoSum {
-
-
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indices = new HashMap<>(); // stores indices of elements in map
 
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            // check if the complement already exists in the map
+            if (indices.containsKey(diff)) {
+                // return that pair if it does
+                return new int[] {indices.get(diff), i};
+            }
+
+            // otherwise add the current num and index to map
+            indices.put(num, i);
+        }
+
+        // fallback if no match found
         return new int[0];
     }
-
 
     public static void main(String[] args) {
         TwoSum solution = new TwoSum();
