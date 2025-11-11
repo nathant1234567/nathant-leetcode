dda658bea8e7a2bebdc0ccee19ed5870abbe10e4
NathanT <nathanthompson256@outlook.com>
2025-11-11 14:20:52 +0000

    add initial project files, made twosum file

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,32 @@
+/**
+ * Problem: Two Sum
+ * Link: https://leetcode.com/problems/two-sum
+ *
+ * Approach:
+ *  - Briefly describe your approach (e.g., "Use HashMap for complement lookup")
+ *
+ * Time Complexity: O(...)
+ * Space Complexity: O(...)
+ */
+
+package arrays;
+
+import java.util.*;
+
+public class TwoSum {
+
+
+    public int[] twoSum(int[] nums, int target) {
+
+        return new int[0];
+    }
+
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
